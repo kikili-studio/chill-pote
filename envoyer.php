b85bb33236985bbf9df98b9390a5e264e92f0b4d
VaroxDev <73996621+Varox-YT@users.noreply.github.com>
2024-07-27 18:42:20 +0200

    Update envoyer.php

--- envoyer.php
+++ envoyer.php
@@ -4,14 +4,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    
     if (empty($nom) || empty($email) || empty($message)) {
         echo "Tous les champs doivent être remplis.";
         exit;
     }
 
-    
-    $to = "beaofficiel@gmail.com";
+    $to = "votre-email@example.com";
     $subject = "Message de Contact de Chill Pote";
     $body = "Nom: $nom\nEmail: $email\n\nMessage:\n$message";
 
